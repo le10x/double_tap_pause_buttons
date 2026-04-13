140a4804c2c388ab4e0b4722217425ce48f41373
le10x <leanderdrb@gmail.com>
2026-04-13 19:58:09 -0400

    Add main.cpp with metadata for Swing Flip

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+{
+    "geode": "5.6.1",
+    "gd": {
+        "win": "2.2081",
+        "android": "2.2081",
+        "mac": "2.2081",
+        "ios": "2.2081"
+    },
+    "id": "le10x.swing_flip",
+    "name": "Swing Flip",
+    "developer": "le10x",
+    "version": "v1.0.0",
+    "description": "Visually inverts the Swing Copter icon when gravity is reversed.",
+    "repository": "https://github.com/le10x/swing_flip"
+}
